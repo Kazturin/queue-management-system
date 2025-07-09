366732afb92932baa2b54b9b5f1bf0cc18c99554
Arman <arman_096@bk.ru>
2025-07-09 10:42:29 +0500

    Telegram bot +

--- resources/views/site/services.blade.php
+++ resources/views/site/services.blade.php
@@ -13,6 +13,14 @@
             </div>
 
             <div class="grid grid-cols-1 gap-6 md:grid-cols-5">
+                <div class="flex justify-start flex-col border-2 border-sky-600 rounded-md">
+                    <div class="p-4 bg-sky-600 text-white font-semibold text-lg uppercase"> Телеграм-бот для помощи абитуриентам </div>
+                    <div class="mx-auto my-6">
+                        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->style('dot')->eye('circle')->merge('/public/img/telegram.png')->size(200)->margin(1)->generate('https://t.me/ShakarimAdmissionBot')) !!} ">
+                        {{--                            {!! QrCode::size(200)->generate(Request::url(route('ticket-create',$service))); !!}--}}
+                    </div>
+                    {{--                        <a href="{{ route('ticket-create',['service'=>$service])}}" target="_blank">go</a>--}}
+                </div>
                 @foreach($services as $service)
                     <div class="flex justify-start flex-col border-2 border-primary-600 rounded-md">
                         <div class="p-4 bg-primary-600 text-white font-semibold text-lg uppercase"> {{$service->name}} </div>
